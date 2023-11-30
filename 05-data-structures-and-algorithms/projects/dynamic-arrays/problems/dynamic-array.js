a655f74e4321ceb6429a99778c56dc8eb4ff0bf0
Sindu Andita Pratama <sindu.andita@gmail.com>
2023-11-30 12:26:53 +0700

    05/project dynamic-arrays

---
+++ 05-data-structures-and-algorithms/projects/dynamic-arrays/problems/dynamic-array.js
@@ -0,0 +1,39 @@
+class DynamicArray {
+  data = null;
+  #length = 0;
+
+  constructor(defaultSize = 4) {
+    // Your code here
+    this.capacity = defaultSize;
+    this.data = Array(this.capacity).fill(undefined);
+  }
+
+  get length() {
+    return this.#length;
+  }
+
+  set length(val) {
+    this.#length = val;
+  }
+
+  read(index) {
+    // Your code here
+    return this.data.at(index);
+  }
+
+  unshift(val) {
+    // Your code here
+    const firstEmptyIndex = this.data.findIndex(el => el === undefined) + 1;
+    this.length++;
+
+    for (let i = firstEmptyIndex; i >= 0; i--) {
+      if (i === 0) {
+        this.data[i] = val;
+      } else {
+        this.data[i] = this.data[i - 1];
+      }
+    }
+  }
+}
+
+module.exports = DynamicArray;
